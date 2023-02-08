c7be4160e83c01975fa0f6004ae12b6affa5d142
alpha-hexor <itsalexwilliams69@gmail.com>
2023-02-08 00:22:05 -0800

    switched to yugen

--- scrapper.py
+++ scrapper.py
@@ -1,117 +1,108 @@
 import httpx
 import re
-import json
 import html
+import base64
 import yarl
 
 #some global shit
-client = httpx.Client(headers={'user-agent':'uwu'},follow_redirects=True,timeout=None)
-EXTENSION = '%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%229c7a8bc1e095a34f2972699e8105f7aaf9082c6e1ccd56eab99c2f1a971152c6%22%7D%7D'
+client = httpx.Client(headers={'user-agent':'uwu'},follow_redirects=True)
 start_regex = r"#EXT-X-STREAM-INF(:.*?)?\n+(.+)"
 res_regex = r"RESOLUTION=\d+x(\d+)"
-key1=b'37911490979715163134003223491201'
-key2 = b'54674138327930866480207815084989'
-iv= b'3134003223491201'
+
 
 class Anime:
     def __init__(self):
-        self.main_url = "https://allanime.site"
-        #self.api_url = "https://blog.allanime.pro/apivtwo/clock.json"
-        self.r = ""
+        self.main_url = "https://yugen.to"
+        self.api_url = "https://yugen.to/api/embed/"
     
-    def search(self,query:str,dct)->list:
+
+    
+    def search(self,query:str)->list:
         """function to search anime
 
         Args:
             query (str): anime name
 
         Returns:
-            list: anime_id and names
+            list: anime_data 
+        Format:
+            anime_date : tuple (anime_id,anime_name)
         """
-        query = query.replace(" ","%20")
-        SEARCH_SLUG = f"%7B%22search%22%3A%7B%22allowAdult%22%3Afalse%2C%22allowUnknown%22%3Afalse%2C%22query%22%3A%22{query}%22%7D%2C%22limit%22%3A26%2C%22page%22%3A1%2C%22translationType%22%3A%22sub%22%2C%22countryOrigin%22%3A%22ALL%22%7D"
-        self.r = client.get(
-            f"{self.main_url}/allanimeapi?variables={SEARCH_SLUG}&extensions={EXTENSION}",
-            headers={'referer':f'{self.main_url}/search-anime'}
-        ).text
+        query = query.replace(" ","+")
+        r=client.get(f"{self.main_url}/discover/?q={query}")
         
-        r=json.loads(self.r)
-        anime_ids = [i['_id'] for i in r['data']['shows']['edges']]
-        names = [i['name'] for i in r['data']['shows']['edges']]
-        #fix the names
-        names = [i.replace(" ","-") for i in names]
-        dct.clear()
-        for anime_id, name in zip(anime_ids, names):
-            dct[anime_id] = name
-        #return anime_ids,names
+        anime_data = re.findall(r'"/anime/(.*?)/(.*?)/"',r.text)
+        
+        return anime_data
     
-    def anime_data(self,index:int)->dict:
+    def anime_data(self,anime_data:tuple)->dict:
         """return all data about anime
 
         Args:
-            index (int): index of the name in the list
+            anime_data : (anime_id,anime_name)
         """
-        r=json.loads(self.r)
-        dub_eps = r['data']['shows']['edges'][index]['availableEpisodes']['dub']
-        sub_eps = r['data']['shows']['edges'][index]['availableEpisodes']['sub']
+        anime_id,name = anime_data
+        r=client.get(
+            f"{self.main_url}/anime/{anime_id}/{name}/watch/"
+        )
+        
+        #collect sub episode
         try:
-            
-            thumbnail_url = r['data']['shows']['edges'][index]['thumbnail']
+            sub_eps = re.findall('<div class="ap-.+?">Episodes</div><span class="description" .+?>(\d+)</span></div>',r.text)[0]
         except:
-            thumbnail_url = None
-        anime_id = r['data']['shows']['edges'][index]['_id']
-        x=client.get(f"{self.main_url}/anime/{anime_id}/")
-        print(x)
-        raw_desc = re.findall(r'description\\":\\"(.*?)\\"',x.text)[0].replace("\\n","").replace("\\","").replace("u003Cbr","").replace("u003E","")
+            #for hall print anime movies
+            sub_eps = "0"
+        
+        #collect dub episode
+        try:
+            dub_eps = re.findall('<div class="ap-.+?">Episodes \(Dub\)</div><span class="description" .+?>(\d+)</span></div>',r.text)[0]
+        except:
+            dub_eps = "0"
+        
+        #collect description
+        raw_desc = re.findall('<p class="description .+?">(.*)</p>',r.text)[0]
+        raw_desc = re.sub(r'<.*?>',"",raw_desc) #clear any html tags
         desc = html.unescape(raw_desc)
+        
+        #collect thumbnail
+        thumbnail_url = re.findall(r'<img src="(.*?)"',r.text)[1]
         
         return {'dub':dub_eps,'sub':sub_eps,'thumbnail':thumbnail_url,'desc':desc}
     
-    def extract_link(self,id:str,name:str,ep:str,mode:str):
+    def extract_link(self,id:str,ep:str,mode:str):
         """get the final streaming link
 
         Args:
-            id (str): allanime anime id
-            name (str): anime to watch
+            id (str): yugene anime id
             ep (str): ep to watch
             mode (str): sub or dub
     
         """
-        r=client.get(
-            f"{self.main_url}/watch/{id}/{name}/episode-{ep}-{mode}"
+        raw_payload = f"{id}|{ep}" if mode == "sub" else f"{id}|{ep}|dub"
+        
+        r=client.post(
+            self.api_url,
+            data={
+                'id':base64.b64encode(raw_payload.encode()).decode(),
+                "ac":"0"
+            },
+            headers={
+                "x-requested-with": "XMLHttpRequest"
+            }
             
-        ).text
+        ).json()
+        streaming_link = r['hls'][0]
+        x = client.get(streaming_link).text
         
-        try:
-            gogo_id =re.findall(r'".*\.php\?id=(.*?)\&.*"',r)[0]
-            
-        except:
-            gogo_id = ""
-        
-        domain,slug,api_id = re.findall(r'"embedUrl":"https://(.*?)/(.*?)/clock\?id=(.*?)&',r)[0]
-        
-        
-        r=client.get(f"https://{domain}/{slug}/clock.json?id={api_id}")
-        
-        
-        
-        try:
-            streaming_links = re.findall(":\s*'(https://.*?)',",str(r.json()))
-        except:
-            streaming_links = re.findall(':\s*"(https://.*?)",',str(r.json()))
-                
        
-        print(streaming_links)
-        for i in streaming_links:
-            if yarl.URL(i).name.endswith('m3u8'):
-                streaming_link = i
-                
-            
-        r=client.get(streaming_link).text
         try:
-            for match in re.finditer(start_regex,r):
+            parent_url = str(yarl.URL(streaming_link).parent)+"/"
+            for match in re.finditer(start_regex,x):
                 res,link = match.groups()
+                if not yarl.URL(link).is_absolute():
+                    link = parent_url+link
                 q=re.findall(res_regex,res)[0]
+
                 yield {q:link}
         except:
             yield {'hls-p':streaming_link}
--- test.py
+++ test.py
@@ -4,16 +4,16 @@ engine = Anime()
 
 
 name = input("[*]Enter anime name: ")
-id,names=engine.search(name)
+anime_data=engine.search(name)
 
-for index,name in enumerate(names,start=1):
-    print(f"{index}: {name}")
 
+for i in range(len(anime_data)):
+    print(f"{i+1}: {anime_data[i][1]}")
+    
 index= (int(input("[*]Enter index: "))-1)
-anime_to_wath = names[index]
-anime_id = id[index]
+anime_to_watch = anime_data[index]
 
-data = engine.anime_data(index)
+data = engine.anime_data(anime_to_watch)
 print(data['dub'])
 print(data['sub'])
 print(data['thumbnail'])
@@ -24,7 +24,6 @@ mode = "sub"
 #print(engine.extract_link(anime_id,anime_to_wath,"1",mode))
 # for item in engine.extract_link(anime_id,anime_to_wath,"1",mode):
 #     print(item)
-x=engine.extract_link(anime_id,anime_to_wath,"1",mode)
+x=engine.extract_link(anime_data[index][0],"1",mode)
 for item in x:
-    print("s")
     print(item)
